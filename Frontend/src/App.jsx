b5aed3e2f6cdd1ff67069ddb823f64bb7e39b7ad
Niranjan0524 <parthalase05gmali.com@gmail.com>
2026-02-28 13:12:17 +0530

    Frontend Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+
+     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 text-2xl font-bold">
+       Welcome to URL Shortner Service!
+     </div>
+    </>
+  )
+}
+
+export default App;
