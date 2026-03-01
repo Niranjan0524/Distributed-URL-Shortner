32e07c187e5b8e2f31e44c7bbd8381b2f83ed085
Niranjan0524 <parthalase05gmali.com@gmail.com>
2026-03-01 22:20:50 +0530

    frontend arch setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header.jsx'
+
 
 function App() {
  
 
   return (
     <>
-
+      <Header />
      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 text-2xl font-bold">
        Welcome to URL Shortner Service!
      </div>
---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header=() => {
+
+    //This component contains the app name in left corner  and in right side signup/login(if not logged in) or profile/logout(if logged in)
+
+    return <>(
+        <div className="header bg-blue-500 text-white p-4 text-center">
+            <h1>URL Shortener</h1>
+        </div>
+    )
+
+    </>
+
+}
+
+export default Header;
