7b11332d599f53a476a33bdcaa4b8782fdfe692b
Niranjan0524 <parthalase05gmali.com@gmail.com>
2026-03-24 19:02:36 +0530

    server folder and db schema setup

---
+++ Backend/cmd/api/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "log/slog"
+
+func main() {
+
+	slog.Info("Welcome to main func")
+
+}
---
+++ Backend/internal/types/types..go
@@ -0,0 +1,25 @@
+package types
+
+import "time"
+
+type Url struct {
+	Id        int64      `json:"id"`
+	ShortCode string     `json:"shortCode" validate:"required"`
+	LongUrl   string     `json:"longUrl" validate:"required,url"`
+	UserId    *string    `json:"userId,omitempty"`
+	CreatedAt time.Time  `json:"createdAt"`
+	ExpiresAt *time.Time `json:"expiresAt,omitempty"`
+}
+
+type Click struct {
+	Id        int64     `json:"id"`
+	UrlId     int64     `json:"urlId" validate:"required"`
+	ClickedAt time.Time `json:"clickedAt"`
+	IpAddress *string   `json:"ipAddress,omitempty"`
+	UserAgent *string   `json:"userAgent,omitempty"`
+}
+
+type CreateUrlRequest struct {
+	LongUrl string  `json:"longUrl" validate:"required,url"`
+	Alias   *string `json:"alias"`
+}
